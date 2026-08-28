d6b3b16c170bb9c2a2268a70fa4e6ad98b304961
David Nicolini <98845165+davidnicolini@users.noreply.github.com>
2026-08-28 16:27:38 +0000

    Adiciona estrutura inicial do projeto com Maven e Spring Boot

---
+++ backend/src/test/java/io/github/davidnicolini/Cadastro_Usuario/CadastroUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.davidnicolini.Cadastro_Usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
